43b27d1f2eb87616ab941e61149ad614f09729fc
Xihao Li <xihaoli@unc.edu>
2024-02-20 15:24:23 -0500

    batch jobs

---
+++ batch jobs/MetaSTAARlite_worker_Gene_Centric_Noncoding_Long_Masks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=3-00:00
+#SBATCH --mem=20000
+#SBATCH --mail-type=NONE
+#SBATCH --array=1-8
+
+module purge
+module load r/4.3.1
+export R_LIBS_USER=/proj/xihaoli_lab/users/xihaoli/R-4.3.1
+R --vanilla --args ${SLURM_ARRAY_TASK_ID} < $1 > "${1}.${SLURM_ARRAY_TASK_ID}.out" 2> "${1}.${SLURM_ARRAY_TASK_ID}.err"
+
---
+++ batch jobs/MetaSTAARlite_worker_Gene_Centric_ncRNA_Long_Masks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=3-00:00
+#SBATCH --mem=20000
+#SBATCH --mail-type=NONE
+
+module purge
+module load r/4.3.1
+export R_LIBS_USER=/proj/xihaoli_lab/users/xihaoli/R-4.3.1
+R --vanilla --args ${SLURM_JOB_ID} < $1 > "${1}.${SLURM_JOB_ID}.out" 2> "${1}.${SLURM_JOB_ID}.err"
+
